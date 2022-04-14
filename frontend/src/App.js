1cecc21701a4ef4875e2fbd5583833bfdaa51155
Amitabh Kale <amitabhkale@gmail.com>
2022-04-14 20:11:28 +0530

    ProductScreen and Rating

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,19 +2,26 @@ import Footer from "./components/Footer";
 import Header from "./components/Header";
 import {Container } from 'react-bootstrap'
 import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen"
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 
 function App() {
   return (
-    <>
+    <Router>
+      
       <Header />
       <main className="py-3">
         <Container>
-        <HomeScreen />
+        {/* <HomeScreen /> */}
+        <Routes>
+        <Route path='/' element={<HomeScreen />} />
+        <Route path='/product/:id' element={<ProductScreen />} />
+        </Routes>
         </Container>
       
       </main>
       <Footer />
-    </>
+    </Router>
     
   );
 }
