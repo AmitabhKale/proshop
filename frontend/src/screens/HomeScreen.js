c0e4684f6134436bdb7fccb791bcff352e89ad9d
Amitabh Kale <amitabhkale@gmail.com>
2022-04-15 11:24:59 +0530

    Added backend and converted es modules

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import { Row, Col } from 'react-bootstrap'
-import products from '../products'
+// import products from '../products'
 import Product from '../components/Product'
+import axios from 'axios'
 
 const Homescreen = () => {
+  
+  const [products,setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const {data} = await axios.get(`/api/products`);
+
+      setProducts(data);
+    }
+    fetchProduct();
+  },[])
+
   return (
     <>
         <h1>Latest Products</h1>
