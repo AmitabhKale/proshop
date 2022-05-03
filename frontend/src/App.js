f779a88098de6b48d61c4ee856edae2c8df984c2
Amitabh Kale <amitabhkale@gmail.com>
2022-05-03 15:48:27 +0530

    Sign In Functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen"
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import CartScreen from "./screens/CartScreen";
+import LoginScreen from "./screens/LoginScreen";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         {/* <HomeScreen /> */}
         <Routes>
         <Route path='/' element={<HomeScreen />} />
+        <Route path='/login' element={<LoginScreen />} />
         <Route path='/product/:id' element={<ProductScreen />} />
         <Route path='/cart' element={<CartScreen />} />
         <Route path='/cart/:id' element={<CartScreen />} />
--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -1,4 +1,5 @@
-import { USER_LOGIN_FAIL, USER_LOGIN_REQUEST } from "../constants/userConstants"
+import { USER_LOGIN_FAIL, USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS, USER_LOGOUT } from "../constants/userConstants"
+import axios from "axios"
 
 export const login = (email, password) => async (dispatch) => {
     try{
@@ -15,6 +16,15 @@ export const login = (email, password) => async (dispatch) => {
         const { data } = await axios.post('/api/users/login', {email, password}, config)
 
         dispatch({
+            type: USER_LOGIN_SUCCESS,
+            payload: data,
+          })
+      
+        localStorage.setItem('userInfo', JSON.stringify(data))
+
+        
+    }catch(error){
+        dispatch({
             type: USER_LOGIN_FAIL,
             payload:
             error.response && error.response.data.message
@@ -23,3 +33,8 @@ export const login = (email, password) => async (dispatch) => {
         })
     }
 }
+
+export const logout = () => (dispatch) => {
+    localStorage.removeItem('userInfo')
+    dispatch({ type: USER_LOGOUT })
+  }
